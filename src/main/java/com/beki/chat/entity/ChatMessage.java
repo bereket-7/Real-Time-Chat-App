2c21eebdafab000e2c077ea4eff734c793c5c217
bereket-7 <bekhonelign@gmail.com>
2024-07-02 04:34:33 +0300

    ChatMessage

---
+++ src/main/java/com/beki/chat/entity/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.beki.chat.entity;
+
+import com.beki.chat.dto.MessageType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
