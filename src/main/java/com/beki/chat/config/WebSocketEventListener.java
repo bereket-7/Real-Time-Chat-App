f1dbc597c1ac2b16188e3e7482896b87824a1131
bereket-7 <bekhonelign@gmail.com>
2024-07-02 05:04:45 +0300

    addUser

--- src/main/java/com/beki/chat/config/WebSocketEventListener.java
+++ src/main/java/com/beki/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.beki.chat.config;
 
+import com.beki.chat.dto.MessageType;
+import com.beki.chat.entity.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,9 +14,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 @RequiredArgsConstructor
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messageSendingOperation;
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        //todo: to be implemented
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username != null) {
+            log.info("User Disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .sender(username)
+                    .type(MessageType.LEAVE)
+                    .build();
+            messageSendingOperation.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
